3315994d0582fc613c4536d319283e285d8d5302
CTRApps <btowner01@gmail.com>
2019-05-02 17:47:55 -0400

    initial commit

---
+++ console/README.md
@@ -0,0 +1,32 @@
+# International Auction Python console app
+
+Using Azure Cognitive Services, this Python console application will serve as a **Proof of Concept** which demonstrates the deployment of the Speech SDK used in speech recognition activities conducted at Live Auctions.  **By using the Cognitive Services Speech SDK you acknowledge its license, see [Speech SDK license agreement](https://aka.ms/csspeech/license201809).**
+
+## Prerequisites
+
+* Python 3.5 or later needs to be installed. Downloads are available [here](https://www.python.org/downloads/).
+* The Speech SDK Python package is available for Windows (x64 and x86)
+* Installation required: [Microsoft Visual C++ Redistributable for Visual Studio 2017](https://support.microsoft.com/help/2977003/the-latest-supported-visual-c-downloads)
+
+## Build the sample
+
+
+* Install the Speech SDK Python package in your Python interpreter, typically by executing the command
+  ```sh
+  pip install azure-cognitiveservices-speech
+  ```
+  in a terminal.
+
+## Run the samples
+
+To run the app, navigate to the `poc/console` directory in your local copy of the this repository.
+Start the app with the command
+
+```sh
+python main.py
+```
+
+Depending on your platform, the Python 3 executable might also just be called `python`.
+
+The app displays a menu that you can navigate using your keyboard.
+Choose the scenarios that you're interested in.
---
+++ console/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 10th Magnitude
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import speech_sdk
+
+from collections import OrderedDict
+import platform
+
+eofkey = 'Ctrl-Z' if "Windows" == platform.system() else 'Ctrl-D'
+print('system: {}, eofkey: {}'.format(platform.system(), eofkey))
+
+samples = OrderedDict([
+    (speech_sdk, [
+        speech_sdk.speech_recognize_once_from_mic,
+        speech_sdk.speech_recognize_once_from_file,
+        speech_sdk.speech_recognize_once_from_file_with_customized_model,
+        speech_sdk.speech_recognize_once_from_file_with_custom_endpoint_parameters,
+        speech_sdk.speech_recognize_async_from_file,
+        speech_sdk.speech_recognize_continuous_from_file,
+        speech_sdk.speech_recognition_with_pull_stream,
+        speech_sdk.speech_recognition_with_push_stream
+    ])
+])
+
+
+def select():
+    modules = list(samples.keys())
+    selected_module = modules[0]
+
+    print('Please select one of the functions using the number to the left of the function, This will enable different ways of accessing the Audio content. Use {} to abort'.format(eofkey))
+    for i, fun in enumerate(samples[selected_module]):
+        print(i, fun.__name__)
+
+    try:
+        num = int(input())
+        selected_function = samples[selected_module][num]
+    except EOFError:
+        raise
+    except Exception as e:
+        print(e)
+        return
+
+    print('You selected: {}'.format(selected_function))
+    try:
+        selected_function()
+    except Exception as e:
+        print('Error running sample: {}'.format(e))
+
+    print()
+
+
+while True:
+    try:
+        select()
+    except EOFError:
+        break
